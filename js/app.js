ba9f83958d83079fbc3889a758b6fa60c10973bd
DuhaAbuGhazleh <duhamahmoud0@gmail.com>
2021-07-27 18:02:21 +0300

    add loop for guess game

--- js/app.js
+++ js/app.js
@@ -4,16 +4,21 @@ let names = prompt('Whats your name?');
 alert ('Welcome to my websit '+ names +' I hope you like it');
 
 alert('I am going to ask you 5 question, to know me more , and your answer should be yes or no ');
+let counter = 0;
 
 let q1 = prompt('DO I  like Anime ?').toLowerCase();
 
 switch(q1){
 case 'yes':
+case 'y':
   alert('NO, I dont like Anime');
-  console.log('yes');
+  //console.log('yes');
   break;
 case 'no':
+case 'n':
+
   alert('Thats right, I dont like anime' );
+  counter ++;
   break;
 default :
   alert('You dont answer my question but I dont like Anime.');
@@ -24,10 +29,15 @@ default :
 let q2 = prompt('DO I  like Spaghetti ?').toLowerCase();
 switch(q2){
 case 'yes':
+case 'y':
+
   alert('yes, I like Spaghetti its the best meal');
-  console.log('yes');
+  counter++ ;
+  //console.log('yes');
   break;
 case 'no':
+case 'n':
+
   alert('sorry  thats wrong,I like it' );
   break;
 default :
@@ -39,10 +49,15 @@ default :
 let q3 = prompt('DO I  like Puzzle ?').toLowerCase();
 switch(q3){
 case 'yes':
+case 'y':
+
+  counter ++ ;
   alert('yes, I like Puzzle');
-  console.log('yes');
+  //console.log('yes');
   break;
 case 'no':
+case 'n':
+
   alert('sorry  thats wrong,Ilike it' );
   break;
 default :
@@ -54,10 +69,14 @@ default :
 let q4 = prompt('DO I  like Codeing ?').toLowerCase();
 switch(q4){
 case 'yes':
+case 'y':
+  counter ++ ;
   alert('yes, I like coding');
-  console.log('yes');
+  //console.log('yes');
   break;
 case 'no':
+case 'n':
+
   alert('sorry  thats wrong, Ilike it' );
   break;
 default :
@@ -70,10 +89,14 @@ default :
 let q5 = prompt('Did I  study software development ?').toLowerCase();
 switch(q5){
 case 'yes':
+case 'y':
+
   alert('Unfortunatly NO');
-  console.log('yes');
+  //console.log('yes');
   break;
 case 'no':
+case 'n':
+  counter ++;
   alert('Thats right but now I am joining a scholarship to learn it' );
   break;
 default :
@@ -84,4 +107,48 @@ default :
 
 alert (' Thanks '+ names +' for your time and I hope you Know me more  by this question.');
 
+alert ('now, I will put a number in my mind and you will try to guess it.');
 
+let mynu = '3' ;
+
+let guessnu = 0;
+
+for (let i = 0 ; i<4|| guessnu === mynu ; i++){
+  guessnu = prompt ('You have 3 chances to guess my number ?');
+
+  if (mynu !== guessnu ){
+    if (guessnu< mynu)
+      alert ('try again , your num too low');
+    else
+      alert ('try again , your num too high');
+
+  }
+
+
+  else {
+    alert ('thats right');
+    counter++ ;
+    break;
+  }
+}
+
+
+alert (' My Num is 3');
+
+
+let multicountry = ['palestine', 'egypt', 'jordan' , 'morocco', 'italy', 'iraq','russia' ];
+let x = 0;
+while (x<6){
+  let typecontry = prompt ('pleas guess my country, you have 6 attempt').toLowerCase();
+  for( let i= 0 ; i <7 ; i ++){
+    if ( multicountry[i]===typecontry ){
+      alert (' GREAT Thats right');
+      counter ++ ;
+      x=x+6;
+      break;
+    }
+  }
+
+  x++;
+}
+alert ('your grade is '+ counter + ' Thanks for your time ');
