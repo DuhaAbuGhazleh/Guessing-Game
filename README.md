61de9fa8e74e77e280eb8229807495aa6f21e154
DuhaAbuGhazleh <duhamahmoud0@gmail.com>
2021-07-28 15:13:23 +0300

    add function

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Guessing-Game
+
 LAB02guessing game about me
+It is just 5 questions to know me more.
+
+And 2 guess questions it is a game thats it.
+
+1- The Driver: Duha Abu-ghazleh
+2- the Navigator : Haia Al lawansah
+
+- What I used in js code
+
+  1- loops  : switch, for and while
+
+  2- If statment
+
+  3- Function
--- js/app.js
+++ js/app.js
@@ -4,151 +4,187 @@ let names = prompt('Whats your name?');
 alert ('Welcome to my websit '+ names +' I hope you like it');
 
 alert('I am going to ask you 5 question, to know me more , and your answer should be yes or no ');
+
+
 let counter = 0;
 
-let q1 = prompt('DO I  like Anime ?').toLowerCase();
+function anime() {
+  let animeq = prompt('DO I  like Anime ?').toLowerCase();
 
-switch(q1){
-case 'yes':
-case 'y':
-  alert('NO, I dont like Anime');
-  //console.log('yes');
-  break;
-case 'no':
-case 'n':
+  switch(animeq){
+  case 'yes':
+  case 'y':
+    alert('NO, I dont like Anime');
+    //console.log('yes');
+    break;
+  case 'no':
+  case 'n':
 
-  alert('Thats right, I dont like anime' );
-  counter ++;
-  break;
-default :
-  alert('You dont answer my question but I dont like Anime.');
-  break;
+    alert('Thats right, I dont like anime' );
+    counter ++;
+    break;
+  default :
+    alert('You dont answer my question but I dont like Anime.');
+    break;
+
+  }
+
 
 }
+anime ();
+// eslint-disable-next-line no-unused-vars
+function food () {
+  let q2 = prompt('DO I  like Spaghetti ?').toLowerCase();
+  switch(q2){
+  case 'yes':
+  case 'y':
 
-let q2 = prompt('DO I  like Spaghetti ?').toLowerCase();
-switch(q2){
-case 'yes':
-case 'y':
+    alert('yes, I like Spaghetti its the best meal');
+    counter++ ;
+    //console.log('yes');
+    break;
+  case 'no':
+  case 'n':
 
-  alert('yes, I like Spaghetti its the best meal');
-  counter++ ;
-  //console.log('yes');
-  break;
-case 'no':
-case 'n':
+    alert('sorry  thats wrong,I like it' );
+    break;
+  default :
+    alert('You dont answer my question but I  like Spaghetti.');
+    break;
 
-  alert('sorry  thats wrong,I like it' );
-  break;
-default :
-  alert('You dont answer my question but I  like Spaghetti.');
-  break;
-
-}
-
-let q3 = prompt('DO I  like Puzzle ?').toLowerCase();
-switch(q3){
-case 'yes':
-case 'y':
-
-  counter ++ ;
-  alert('yes, I like Puzzle');
-  //console.log('yes');
-  break;
-case 'no':
-case 'n':
-
-  alert('sorry  thats wrong,Ilike it' );
-  break;
-default :
-  alert('You dont answer my question but I  like Puzzle.');
-  break;
+  }
 
 }
+food () ;
+// eslint-disable-next-line no-unused-vars
+function puzzle() {
+  let q3 = prompt('DO I  like Puzzle ?').toLowerCase();
+  switch(q3){
+  case 'yes':
+  case 'y':
 
-let q4 = prompt('DO I  like Codeing ?').toLowerCase();
-switch(q4){
-case 'yes':
-case 'y':
-  counter ++ ;
-  alert('yes, I like coding');
-  //console.log('yes');
-  break;
-case 'no':
-case 'n':
+    counter ++ ;
+    alert('yes, I like Puzzle');
+    //console.log('yes');
+    break;
+  case 'no':
+  case 'n':
 
-  alert('sorry  thats wrong, Ilike it' );
-  break;
-default :
-  alert('You dont answer my question but I  like Coding.');
-  break;
+    alert('sorry  thats wrong,Ilike it' );
+    break;
+  default :
+    alert('You dont answer my question but I  like Puzzle.');
+    break;
 
-}
-
-
-let q5 = prompt('Did I  study software development ?').toLowerCase();
-switch(q5){
-case 'yes':
-case 'y':
-
-  alert('Unfortunatly NO');
-  //console.log('yes');
-  break;
-case 'no':
-case 'n':
-  counter ++;
-  alert('Thats right but now I am joining a scholarship to learn it' );
-  break;
-default :
-  alert('You dont answer my question ');
-  break;
+  }
 
 }
+puzzle ();
+
+// eslint-disable-next-line no-unused-vars
+function likeCode () {
+  let q4 = prompt('DO I  like Codeing ?').toLowerCase();
+  switch(q4){
+  case 'yes':
+  case 'y':
+    counter ++ ;
+    alert('yes, I like coding');
+    //console.log('yes');
+    break;
+  case 'no':
+  case 'n':
+
+    alert('sorry  thats wrong, Ilike it' );
+    break;
+  default :
+    alert('You dont answer my question but I  like Coding.');
+    break;
+
+  }
+
+
+}
+likeCode () ;
+
+// eslint-disable-next-line no-unused-vars
+function study() {
+
+  let q5 = prompt('Did I  study software development ?').toLowerCase();
+  switch(q5){
+  case 'yes':
+  case 'y':
+
+    alert('Unfortunatly NO');
+    //console.log('yes');
+    break;
+  case 'no':
+  case 'n':
+    counter ++;
+    alert('Thats right but now I am joining a scholarship to learn it' );
+    break;
+  default :
+    alert('You dont answer my question ');
+    break;
+
+  }
+}
+study();
 
 alert (' Thanks '+ names +' for your time and I hope you Know me more  by this question.');
 
 alert ('now, I will put a number in my mind and you will try to guess it.');
 
-let mynu = '3' ;
 
-let guessnu = 0;
+// eslint-disable-next-line no-unused-vars
+function guessmynumber () {
 
-for (let i = 0 ; i<4|| guessnu === mynu ; i++){
-  guessnu = prompt ('You have 3 chances to guess my number ?');
+  let mynu = '3' ;
 
-  if (mynu !== guessnu ){
-    if (guessnu< mynu)
-      alert ('try again , your num too low');
-    else
-      alert ('try again , your num too high');
+  let guessnu = 0;
 
-  }
+  for (let i = 0 ; i<4|| guessnu === mynu ; i++){
+    guessnu = prompt ('You have 4 chances to guess my number ?');
 
+    if (mynu !== guessnu ){
+      if (guessnu< mynu)
+        alert ('try again , your num too low');
+      else
+        alert ('try again , your num too high');
 
-  else {
-    alert ('thats right');
-    counter++ ;
-    break;
-  }
-}
+    }
 
 
-alert (' My Num is 3');
-
-
-let multicountry = ['palestine', 'egypt', 'jordan' , 'morocco', 'italy', 'iraq','russia' ];
-let x = 0;
-while (x<6){
-  let typecontry = prompt ('pleas guess my country, you have 6 attempt').toLowerCase();
-  for( let i= 0 ; i <7 ; i ++){
-    if ( multicountry[i]===typecontry ){
-      alert (' GREAT Thats right');
-      counter ++ ;
-      x=x+6;
+    else {
+      alert ('thats right');
+      counter++ ;
       break;
     }
   }
 
-  x++;
+
+  alert (' My Num is 3');
 }
+guessmynumber ();
+
+// eslint-disable-next-line no-unused-vars
+function favcountry () {
+
+  let multicountry = ['palestine', 'egypt', 'jordan' , 'morocco', 'italy', 'iraq','russia' ];
+  let x = 0;
+  while (x<6){
+    let typecontry = prompt ('pleas guess my country, you have 6 attempt').toLowerCase();
+    for( let i= 0 ; i <7 ; i ++){
+      if ( multicountry[i]===typecontry ){
+        alert (' GREAT Thats right');
+        counter ++ ;
+        x=x+6;
+        break;
+      }
+    }
+
+    x++;
+  }
+}
+favcountry () ;
+
 alert ('your grade is '+ counter + ' Thanks for your time ');
